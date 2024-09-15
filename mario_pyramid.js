e88e8ae637e84548053b8d5ce9a0f6630ad96437
Max <max.k.ruholl@gmail.com>
2024-09-14 20:24:10 -0500

    render pyramid to the DOM instead of the console

--- mario_pyramid.js
+++ mario_pyramid.js
@@ -30,16 +30,22 @@ function printPyramid(height) {
         
         for(let j=0; j<rows-i; j++){//inner loop
       
-          str += " "; //this adds a space to the end of the string.
+          str += "."; //this adds a space to the end of the string.
           
         }
         for(let k=0; k<i; k++){//inner loop
       
-            str += "#"; //this adds a # to the end of the string.
+          str += "#"; //this adds a # to the end of the string.
              
         }
         str += "#\n"; //adds a # to the string so that the top doesn't end with only 1 #
-         
+        
       }
       console.log(str);  //once a row is done we print it out
-}
+      
+      const para = document.createElement("p");
+      const node = document.createTextNode(str);
+      para.appendChild(node);
+      const element = document.getElementById("pyramid");
+      element.appendChild(para);
+    }
